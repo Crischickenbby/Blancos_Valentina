ca1fcc18bb05c176bc3ac2cd3af56ef2bab0c41d
Chicken <cristiankantun196@gmail.com>
2025-11-26 01:04:11 -0600

    Arregle lo de la configuracion de la base de datos

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ from dotenv import load_dotenv #esto es para cargar las variables de entorno des
 load_dotenv()
 
 # Configuración de la base de datos
-DB_HOST = os.getenv('DB_HOST', 'localhost') #os es para Windows getenv es para Linux pero estoy utilizando Windows y funciona
-DB_NAME = os.getenv('DB_NAME', 'Blancos_Valentina_Ataque')
-DB_USER = os.getenv('DB_USER', 'postgres')
-DB_PASSWORD = os.getenv('DB_PASSWORD', '')
+DB_HOST = os.getenv('DB_HOST') #os es para Windows getenv es para Linux pero estoy utilizando Windows y funciona
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
 
 # Clave secreta para Flask
-SECRET_KEY = os.getenv('SECRET_KEY', 'clave-por-defecto') #esto sirve para que no se vea la clave en el código de la aplicación y se mantenga segura osea en palabras mas sencillas es para que no se vea la clave en el código de la aplicación y se mantenga segura
+SECRET_KEY = os.getenv('SECRET_KEY') #esto sirve para que no se vea la clave en el código de la aplicación y se mantenga segura osea en palabras mas sencillas es para que no se vea la clave en el código de la aplicación y se mantenga segura
 #con codigo de la aplicación me refiero a que es el código que se ejecuta en el servidor
 
 # Función para obtener una conexión a la base de datos
